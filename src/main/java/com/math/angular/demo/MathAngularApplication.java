b2438af418581290ade00e3411318ed2b6f1b40d
jeremytouch <kenghtt@gmail.com>
2019-07-23 19:49:28 -0500

    Initial commit

---
+++ src/main/java/com/math/angular/demo/MathAngularApplication.java
@@ -0,0 +1,13 @@
+package com.math.angular.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/math/angular/demo/MathAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.math.angular.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MathAngularApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
