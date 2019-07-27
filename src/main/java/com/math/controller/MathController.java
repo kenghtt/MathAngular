d7ec1e4433f57137390033612116e830f030a6bf
jeremytouch <kenghtt@gmail.com>
2019-07-27 17:45:15 -0500

    Generating Math Questions Correctly

--- src/main/java/com/math/controller/MathController.java
+++ src/main/java/com/math/controller/MathController.java
@@ -1,8 +1,15 @@
 package com.math.controller;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Project: MathAngular
@@ -24,6 +31,84 @@ public class MathController {
     @ResponseBody
     public String sayHello() {
         return "Hello World";
+    }
+
+    @GetMapping("/mathQuestions")
+    @ResponseBody
+    public String mathQuestions() throws JSONException {
+
+        int max = 5;
+        int min = 1;
+//        int x;
+//        int y;
+//        int[] questionArray;
+//        int[] answersArray = new int[4];
+//        int randomAssign;
+//        int answer;
+        JSONArray jsonArray = new JSONArray();
+        JSONObject finalObject = new JSONObject();
+
+
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 3; i++) {
+            int x;
+            int y;
+            int[] questionArray;
+            int[] answersArray = new int[4];
+            int randomAssign;
+            Random r = new Random();
+            x = r.nextInt((max - min) + 1) + min;
+            y = r.nextInt((max - min) + 1) + min;
+            int answer = x + y;
+
+            randomAssign = r.nextInt((3 - 0) + 1);
+            if (randomAssign == 0) {
+                answersArray[0] = answer;
+                answersArray[1] = answer + 1;
+                answersArray[2] = answer + 2;
+                answersArray[3] = answer + 3;
+            } else if (randomAssign == 1) {
+                answersArray[0] = answer - 1;
+                answersArray[1] = answer;
+                answersArray[2] = answer + 1;
+                answersArray[3] = answer + 2;
+            } else if (randomAssign == 2) {
+                answersArray[0] = answer - 2;
+                answersArray[1] = answer - 1;
+                answersArray[2] = answer;
+                answersArray[3] = answer + 1;
+            } else if (randomAssign == 3) {
+                answersArray[0] = answer - 3;
+                answersArray[1] = answer - 2;
+                answersArray[2] = answer - 1;
+                answersArray[3] = answer;
+            }
+
+
+            questionArray = new int[]{x, y, answer};
+
+            arrayList.add(0, answersArray);
+            arrayList.add(0, questionArray);
+
+            HashMap<int[], int[]> hashMap = new HashMap<>();
+            hashMap.put(questionArray, answersArray);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("x", x);
+            jsonObject.put("y", y);
+            jsonObject.put("Answer", answer);
+            if(answersArray[0] == 0 && answersArray[1]==0 && answersArray[2]==0&& answersArray[3]==0){
+                System.out.println("000");
+            }
+            jsonObject.put("potentialAnswer1", answersArray[0]);
+            jsonObject.put("potentialAnswer2", answersArray[1]);
+            jsonObject.put("potentialAnswer3", answersArray[2]);
+            jsonObject.put("potentialAnswer4", answersArray[3]);
+
+            finalObject.put("MathQuestions", jsonArray.put(jsonObject));
+        }
+
+        return finalObject.toString();
     }
 
 }
