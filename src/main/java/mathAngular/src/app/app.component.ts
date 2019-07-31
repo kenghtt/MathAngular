1baf6117c0ba0d7787c1a22e114a6a9ea3e1769e
jeremytouch <kenghtt@gmail.com>
2019-07-31 00:13:41 -0500

    added countdown timer

--- src/main/java/mathAngular/src/app/app.component.ts
+++ src/main/java/mathAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {delay, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -30,25 +31,45 @@ export class AppComponent {
   currentQuestion = 1;
   currentQuestionArray;
   numOfCorrectAnswers = 0;
+  countDownNumber = 30;
+
 
   checkAnswer(event){
     if(event.target.innerText == this.answer){
       this.numOfCorrectAnswers++;
     }
+    console.log(" "+this.answer);
     console.log("Correct Answers: "+this.numOfCorrectAnswers);
   }
 
+  startCountdown(){
+
+    var interval = setInterval(() => {
+      console.log(this.countDownNumber);
+      this.countDownNumber--;
+
+
+      if(this.countDownNumber < 0 ){
+
+        // The code here will run when
+        // the timer has reached zero.
+
+        clearInterval(interval);
+        console.log('Ding!');
+      };
+    }, 1000);
+  };
 
 
 
   getNextQuestion() {
-    this.answer = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["Answer"]
-    this.x = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["x"]
-    this.y = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["y"]
-    this.potentialAnswer1 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer1"]
-    this.potentialAnswer2 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer2"]
-    this.potentialAnswer3 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer3"]
-    this.potentialAnswer4 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer4"]
+    this.answer = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["Answer"];
+    this.x = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["x"];
+    this.y = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["y"];
+    this.potentialAnswer1 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer1"];
+    this.potentialAnswer2 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer2"];
+    this.potentialAnswer3 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer3"];
+    this.potentialAnswer4 = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion]["potentialAnswer4"];
 
     this.currentQuestionArray = this.response.MathQuestions[this.currentArray]["Question " + this.currentQuestion];
     // this.someAnswer = this.potentialAnswer1 = this.response.MathQuestions[this.currentQuestion];
