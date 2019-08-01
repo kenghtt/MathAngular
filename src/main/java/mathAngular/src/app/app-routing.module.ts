ddeb2e1e84fab1c969879e4cf615c5714b762e37
jeremytouch <kenghtt@gmail.com>
2019-08-01 00:23:26 -0500

    working with colors

--- src/main/java/mathAngular/src/app/app-routing.module.ts
+++ src/main/java/mathAngular/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PlayComponent } from './play/play.component';
+import {HighScoreComponent} from "./high-score/high-score.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'play', component: PlayComponent },
+  { path: 'high-score', component: HighScoreComponent },
+
 
 
 ];
